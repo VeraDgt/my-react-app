d0a0588b39d760351fa44200d2f39fade6303ab0
VeraDgt <veradgt@gmail.com>
2022-12-01 09:19:25 +0300

    hook useEffect in Settings

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,18 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function Settings() {
-  const [count, setCount] = useState(0);
+const Settings = ()=> {
+  const [advice, setAdvice] = useState("");
+  useEffect(()=>{
+    const url = "https://api.adviceslip.com/advice";
 
-  return (
-    <div className="row">
-      <div className="col-6"><p>Количество кликов: {count}</p></div>
-      <div className="col-6">
-        <button className="btn btn-warning" onClick={()=>{
-          setCount(count + 1);
-        }}>Нажми тут</button>
-      </div>
-    </div>
-  )
-}
+    const fetchData = async()=> {
+      try {
+        let response = await fetch(url);
+        let json = await response.json();
+        setAdvice(json.slip.advice);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    fetchData();
+  }, []);
+  return <div>{advice}</div>;
+};
+
+// function Settings() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div className="row">
+//       <div className="col-6"><p>Количество кликов: {count}</p></div>
+//       <div className="col-6">
+//         <button className="btn btn-warning" onClick={()=>{
+//           setCount(count + 1);
+//         }}>Нажми тут</button>
+//       </div>
+//     </div>
+//   )
+// }
 
 export default Settings;
